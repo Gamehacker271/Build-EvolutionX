e791296b85547f403afee9656844c1a3b4fe5cb5
WhoFoss <willscheyler@gmail.com>
2026-05-27 13:57:09 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -211,8 +211,8 @@ mkdir -p out/target/product/sapphire/obj/KERNEL_OBJ/usr
 # ================================
 # Build ROM
 # ================================
-export WITH_MICROG=true
-export WITH_GMS=true
+# export WITH_MICROG=true
+# export WITH_GMS=true
 brunch sapphire user || error_exit "Brunch failed"
 
 print_header "Build process completed successfully!"
